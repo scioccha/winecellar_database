63358d7c36558614d8e6eb9860cc1e67177c42d7
Alexandra Sciocchetti <76969118+scioccha@users.noreply.github.com>
2022-05-11 15:01:44 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+from logging.config import IDENTIFIER
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
+import os
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_XXXX'
+app.config['MYSQL_PASSWORD'] = #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_XXXX'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+
+mysql = MySQL(app)
+
+
+# Routes
+# have homepage route to /people by default for convenience, generally this will be your home route with its own template
+@app.route("/")
+def home():
+    return redirect("/wines")
+
+
+# route for wines page
+@app.route("/wines", methods=["POST", "GET"])
+def wines():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a new wine
+    if request.method == "POST":
+        if request.form.get("Add_Wine"):
+
+            # grab user form inputs
+            vintage = request.form["vintage"]
+            vineyard = request.form["vineyard"]
+            variety = request.form["variety"]
+
+            # add data
+            query = "INSERT INTO Wines (vintage, vineyard, variety) VALUES (%s, %s,%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (vintage, vineyard, variety))
+            mysql.connection.commit()
+
+
+            # redirect back to people page
+            return redirect("/wines")
+
+    # Grab bsg_people data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all wines in Wines
+        query = "SELECT wineID, vintage, vineyard, variety FROM Wines"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("wines.j2", data=data)
+
+
+# route for delete functionality
+# we want to pass the 'id' value of that wine on button click (see HTML) via the route
+@app.route("/delete_wines/<int:wineID>")
+def delete_wine(wineID):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Wines WHERE wineID = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (wineID,))
+    mysql.connection.commit()
+
+    # redirect back to wines
+    return redirect("/wines")
+
+
+
+# Listener
+# change the port number if deploying on the flip servers
+if __name__ == "__main__":
+    app.run(port=5227, debug=True)
---
+++ winecellar.sql
@@ -0,0 +1,101 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE TABLE Wines(
+    wineID INT(11) NOT NULL AUTO_INCREMENT,
+    vintage year(4) NOT NULL,
+    vineyard VARCHAR(45) NOT NULL,
+    variety VARCHAR(45) NOT NULL,
+    PRIMARY KEY(wineID)
+);
+
+CREATE TABLE Winemakers(
+    winemakerID INT(11) NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    location VARCHAR(45) NOT NULL,
+    PRIMARY KEY(winemakerID)
+);
+
+CREATE TABLE Winemaker_Details(
+    winemakerDetailsID INT(11) NOT NULL AUTO_INCREMENT,
+    winemakerID INT(11) NOT NULL,
+    wineID int(11) NOT NULL,
+    PRIMARY KEY(winemakerDetailsID),
+    FOREIGN KEY (winemakerID) REFERENCES Winemakers(winemakerID)
+    ON DELETE CASCADE,
+    FOREIGN KEY (wineID) REFERENCES Wines(wineID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Invoices(
+    invoiceID INT(11) NOT NULL AUTO_INCREMENT,
+    wineID INT(11),
+    dateReceived DATE NOT NULL,
+    price decimal(19,2),
+    quantityGallons INT(11),
+    PRIMARY KEY(invoiceID),
+    FOREIGN KEY (wineID) REFERENCES Wines(wineID)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE WorkOrders(
+    workOrderID INT(11) NOT NULL AUTO_INCREMENT,
+    task VARCHAR(400) NOT NULL,
+    winemakerID INT(11),
+    dateOrdered DATE NOT NULL,
+    status enum('Incomplete','Complete') NOT NULL DEFAULT 'Incomplete',
+    PRIMARY KEY(workOrderID),
+    FOREIGN KEY (winemakerID) REFERENCES Winemakers(winemakerID)
+    ON DELETE SET NULL
+);
+
+--
+-- Add Data 
+--
+
+INSERT INTO Wines (vintage, vineyard, variety) 
+VALUES
+(2019, 'Philo Wines', 'Zinfandel'),
+(2020, 'Napa Special', 'Pinot Noir'),
+(2021, 'Monterey Majestic', 'Gewürztraminer'),
+(2020, 'Anderson Valley', 'Nerello Mascalese'),
+(2021, 'Confluence Vineyard', 'Chardonnay');
+
+INSERT INTO Winemakers (firstName, lastName, location) 
+VALUES
+('Alex', 'Ryan', 'Napa, CA'),
+('Michael', 'Fay', 'Philo, CA'),
+('Bruce', 'Zoecklein', 'Walla Walla, WA'),
+('Erin', 'Amaral', 'Willamette, OR'),
+('Jamie', 'Leffert', 'Santa Ynez, CA');
+
+INSERT INTO Invoices (wineID, dateReceived, price, quantityGallons) 
+VALUES
+(4, '2022-01-15', '80000.00', 500),
+(5, '2022-01-15', '200000.00', 2000),
+(1, '2022-03-14', '500000.00', 4500),
+(3, '2022-03-25', '425000.00', 3750),
+(2, '2021-12-29', '90000.00', 650),
+(2, '2022-01-13', '100000.00', 750);
+
+
+INSERT INTO Winemaker_Details (winemakerID, wineID) 
+VALUES
+(2, 1),
+(3, 1),
+(2, 2),
+(1, 3),
+(5, 4),
+(4, 4),
+(4, 5);
+
+
+INSERT INTO WorkOrders (task, winemakerID, dateOrdered) 
+VALUES
+('Take sugar readings from 9/1/2021 -> 9/15/2021 for tanks 2-10', 4, '2021-08-05'),
+('Check SO2 and vineyard PWines manager with monthly update ', 1, '2021-08-12'),
+('Perform pump overs in Tank Hall 4', 5, '2021-08-21'),
+('Fill barrels for Michael and add notes on procedure before vacation.', 2, '2021-09-02'),
+('Take samples for blending trial and document process for new mix Chardonnay', 3, '2021-09-18');
+
+SET FOREIGN_KEY_CHECKS=1;
