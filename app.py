c42d13b845088b9ac9e84ad8ee26b39296d2e3c8
Victoria Patterson <pattersv@flip3.engr.oregonstate.edu>
2022-05-18 07:42:38 -0700

    added routes for invoice page

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import os
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = 'cs340_XXXX'
-app.config['MYSQL_PASSWORD'] = #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_XXXX'
+app.config['MYSQL_USER'] = 'cs340_pattersv'
+app.config['MYSQL_PASSWORD'] =  '7058'#last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_pattersv'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 
@@ -23,7 +23,8 @@ def home():
     return redirect("/wines")
 
 
-# route for wines page
+###### route for wines page ######
+
 @app.route("/wines", methods=["POST", "GET"])
 def wines():
     # Separate out the request methods, in this case this is for a POST
@@ -71,9 +72,49 @@ def delete_wine(wineID):
     # redirect back to wines
     return redirect("/wines")
 
+###### END route for wines page ######
+
+###### route for invoices page ######
+@app.route("/invoices", methods=["POST", "GET"])
+def invoices():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a new wine
+    if request.method == "POST":
+        if request.form.get("Add_Invoice"):
+
+            # grab user form inputs
+            dateReceived = request.form["Date Received"]
+            price = request.form["Price"]
+            quantityGallons = request.form["Quantity (gallons)"]
+
+            # add data
+            query = "INSERT INTO Invoices (dateReceived, price, quantityGallons) VALUES (%s, %s,%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (dateReceived, price, quantityGallons))
+            mysql.connection.commit()
+
+
+            # redirect back to people page
+            return redirect("/invoices")
+
+    # Grab bsg_people data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all wines in Wines
+        query = "SELECT invoiceID, wineID, dateReceived, price, quantityGallons FROM Invoices"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("invoices.j2", data=data)
+
+###### END route for invoices page ######
+
+
+
 
 
 # Listener
 # change the port number if deploying on the flip servers
 if __name__ == "__main__":
-    app.run(port=5227, debug=True)
+    app.run(port=1227, debug=True)
