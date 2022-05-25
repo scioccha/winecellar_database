9caab7a65db005d026bff0fee973ac3303a675ae
Victoria Patterson <vrpatter@gmail.com>
2022-05-25 09:53:30 -0700

    working on null bug

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def workOrders():
             winemakerID = request.form["winemakerID"]
             dateOrdered = request.form["dateOrdered"]
 
-            if winemakerID == "" or winemakerID == "0" or winemakerID == "None":
+            if winemakerID == "":
                 # add data
                 query = "INSERT INTO WorkOrders (task, dateOrdered) VALUES (%s,%s)"
                 cur = mysql.connection.cursor()
@@ -259,7 +259,7 @@ def edit_workOrder(workOrderID):
             winemakerID = request.form["winemakerID"]
             dateOrdered = request.form["dateOrdered"]
 
-            if winemakerID == "" or winemakerID == "0" or winemakerID == "None":
+            if winemakerID == "":
                 query = "UPDATE WorkOrders SET WorkOrders.task = %s, WorkOrders.winemakerID = NULL, WorkOrders.dateOrdered = %s WHERE WorkOrders.workOrderID = %s"
                 cur = mysql.connection.cursor()
                 cur.execute(query, (task, dateOrdered, workOrderID))
